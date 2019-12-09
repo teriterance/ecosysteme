d1def8bb72ebfd615c34b12e980c43a72c47508f
Sh4wy <anthonin.martinel@ensta-bretagne.org>
2019-12-09 14:25:11 +0100

    update_0912_1424
    update 09/12/2019 14h25

--- src/sample/Carnivore.java
+++ src/sample/Carnivore.java
@@ -1,4 +1,40 @@
 package sample;
+import java.util.*;
+import java.util.LinkedList;
 
-public class Carnivore {
+/** ATTENTION : valeurs à modifier par leur valeurs numériques :
+ * ValnbFaim
+ * ValnbSoif
+ */
+
+
+public class Carnivore extends Animal {
+
+    public Carnivore(int x, int y) {
+        super(ValnbFaim, ValnbSoif, x, y);
+    }
+
+    public int chercheProie(int listeAnimaux) {
+        /** Fonction a appeler des que le carnivore cherche une prois à manger:
+         * Il choisit sa cible en prenant la proie avec le produit endurance*distance le plus faible
+         * Entrees : liste des animaux
+         * Sorties : l'ID de la cible
+         *
+         */
+
+        /** ID de la cible en cours */
+        int cible;
+
+        /** enduranceCible*distance min */
+        int min;
+
+        // Parcours de la liste des animaux
+        java.util.ListIterator lIterator = listeAnimaux.listIterator();
+        while (lIterator.hasNext()) {
+            Animal cible = lIterator.next();
+            if cible
+        }
+
+
+    }
 }
