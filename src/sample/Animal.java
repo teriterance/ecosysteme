7adc9f08bd62789132fd1c1e491abc8ab7d0a632
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2019-12-11 15:10:43 +0100

    J'ai rajoute les fonctions de deplacement vers la cible point d'eau, on s'en inspirerea pour la fonction de poursuite d'animal pour un carnivore

--- src/sample/Animal.java
+++ src/sample/Animal.java
@@ -30,9 +30,12 @@ public abstract class Animal {
     private int vitesse;
     /**Pour savoir son etat de recherche d'eau **/
     private  boolean eau_oui_non = true;// vrais si il cherche de l'eau, faux si il a deja une direction
-    /**vecteur de direction de recherche **/
-    private int direction_x;
-    private  int direction_y;
+    /**la le point d'eau que l'on cherche **/
+    private int position_eau_x = -1;
+    private  int position_eau_y = -1;
+    private float rayon_eau = -1;
+    /**on presente l'adresse du point d'eau qui est visee**/
+    private int id_point_eau_vise = -1;
 
     /**definition des constantes statiques leurs valeur seront redefinies pour les different heritiers de la classe animal**/
 
@@ -57,7 +60,7 @@ public abstract class Animal {
         this.soif = nbSoif;
     }
 
-    void manger(int val_nouriture){
+    public void manger(int val_nouriture){
         //nourriture est une valeur a modifier selon l'heritier de la fonction
         this.faim += val_nouriture;
         if (this.faim > this.faim_max){
@@ -65,7 +68,7 @@ public abstract class Animal {
         }
     }
 
-    void deplacer(int x, int y){
+    public void deplacer(int x, int y){
         //x et y sont les valeur de deplacement selon x et y
         if ((x <= this.vitesse) && (x <= this.vitesse)) {
             this.abscisse += x;
@@ -76,14 +79,14 @@ public abstract class Animal {
             throw new IllegalArgumentException("Les valeurs des deplacement sont plus grandesque les valeur vitesse");
         }
     }
-    void boire(int val_eau){
+    public void boire(int val_eau){
         //valeur d'eau a boire
         this.soif += val_eau;
         if(this.soif > this.soif_max){
             this.soif = this.soif_max;
         }
     }
-    void courir(int x, int y){
+    public void courir(int x, int y){
         //ici on definira un deplacement avec une vitesse hautes
         if ((x <= this.vitesseMax) && (y <= this.vitesseMax)) {
             this.abscisse += x;
@@ -94,12 +97,11 @@ public abstract class Animal {
             throw new IllegalArgumentException("Les valeurs de deplacement sont plus grandes que les valeurs vitesse");
         }
     }
-
-    boolean est_mort(){
+    public boolean est_mort(){
         //si le nombre de points de vie est inferieur a zero tu es mort
         return (point_de_vie <= 0 );
     }
-    void recois_attque(int val_attaque){
+    public void recois_attque(int val_attaque){
         // il donne la valeur de son attaque a l'adversaire
         if (!this.est_mort()){
             this.point_de_vie = val_attaque;
@@ -110,20 +112,45 @@ public abstract class Animal {
         }
     }
 
-    void meurt(){
+    public void meurt(){
+        //fonction de mort
         this.point_de_vie = 0;
     }
-    double dist(Poin_eau e){
-        return Math.sqrt(Math.pow(this.abscisse - e.get_abscisse(), 2) + Math.pow(this.ordonnee - e.get_ordonnee(), 2));
+
+    public boolean deplace_vers_point_eau(){
+        /**cas de la direction definit, l'animal ne cherche plus de point d'eau**/
+        double a = Math.sqrt(this.position_eau_y*this.position_eau_y + this.position_eau_x*this.position_eau_x);
+        this.abscisse += (int)(this.abscisse - this.position_eau_x)*this.vitesse/a;
+        this.ordonnee += (int)(this.ordonnee - this.position_eau_y)*this.vitesse/a;
+        if (Math.pow((this.abscisse - this.position_eau_x),2) + Math.pow((this.abscisse - this.position_eau_x),2) < Math.pow(this.rayon_eau,2)){
+            return false;
+        }
+        return true;
     }
 
-    void chercher_a_boire(ArrayList<Poin_eau> eaus) {
+    public boolean chercher_a_boire(ArrayList<Poin_eau> eaus) {
         /**Cherche le point d'eau le plus proche, si pas de point d'eau dans le champ de perception,
          * choisit une direction au hasard et s'avance dans cette direction **/
-
+        if ((this.position_eau_x != -1) && (this.position_eau_y != -1)){
+            return false;
+        }
+        if(!((this.position_eau_x == -1 ) ^ (this.position_eau_y == -1 ))){
+            throw new IllegalArgumentException("erreur de signe, la position du point d'eau ne peut pas etre negative");
+        }
+        /**cas ou on initialise les choses **/
+        float dist_min = 100000;
+        for (int counter = 0 ; counter < eaus.size() ; counter++){
+            double a = Math.pow((this.abscisse - this.position_eau_x),2) + Math.pow((this.abscisse - this.position_eau_x),2);
+            if ( a < dist_min){
+                this.position_eau_x = eaus.get(counter).get_abscisse();
+                this.position_eau_y = eaus.get(counter).get_ordonnee();
+                this.rayon_eau = eaus.get(counter).getRayon();
+            }
+        }
+        return true;
     }
 
-    void meurt_de_faimsoif(int viem, int endm) {
+    public void meurt_de_faimsoif(int viem, int endm) {
         /** Si l'animal a faim ou soif il perd de la vie et de l'endurance jusqu'a mourir
          * on prend en compte les cas où ls points de vie ou l'ndurance peuvent être inferieurs a 0*/
         int diffpdv = this.point_de_vie - viem;
@@ -149,7 +176,7 @@ public abstract class Animal {
             this.endurance = 0;
         }
     }
-    int attaque(){
+    public int attaque(){
         /**avec quelle puissance il attaque**/
         return  this.point_attaque;
     }
--- src/sample/Poin_eau.java
+++ src/sample/Poin_eau.java
@@ -1,22 +1,28 @@
 package sample;
 
 public class Poin_eau {
-    private int abscisse;
-    private  int ordonnee;
-    private  float rayon;
+    /**Le nombre de point d'eau**/
+    private static  int nb_point_eau = 0;
 
-    public  Poin_eau(int x, int y, float rayon){
+    /**les variables de classe**/
+    /**coordone du centre du point d'eau et son rayon**/
+    private int abscisse;
+    private int ordonnee;
+    private float rayon;
+    private int id_point_eau = -1;
+
+    /*constructeur**/
+    public Poin_eau(int x, int y, float rayon){
         this.abscisse = x;
         this.ordonnee = y;
         this.rayon = rayon;
+        id_point_eau += nb_point_eau;
+        nb_point_eau ++;
     }
 
-    //pas encore de constructeur
-    public float getRayon() {return  this.rayon;}
-    public int get_abscisse(){
-        return  this.abscisse;
-    }
-    public int get_ordonnee(){
-        return  this.ordonnee;
-    }
+    /**definition des geters **/
+    public float getRayon() {   return  this.rayon;  }
+    public int get_abscisse(){  return  this.abscisse;  }
+    public int get_ordonnee(){ return  this.ordonnee; }
+    public int get_id_point_eau(){ return  this.id_point_eau; }
 }
