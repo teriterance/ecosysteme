45189c28c6157ff11ad169769c4d985296bcb9d9
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2019-12-11 13:48:15 +0100

    introduction du point d'eau

--- src/sample/Poin_eau.java
+++ src/sample/Poin_eau.java
@@ -3,13 +3,19 @@ package sample;
 public class Poin_eau {
     private int abscisse;
     private  int ordonnee;
+    private  float rayon;
+
+    public  Poin_eau(int x, int y, float rayon){
+        this.abscisse = x;
+        this.ordonnee = y;
+        this.rayon = rayon;
+    }
 
     //pas encore de constructeur
-
+    public float getRayon() {return  this.rayon;}
     public int get_abscisse(){
         return  this.abscisse;
     }
-
     public int get_ordonnee(){
         return  this.ordonnee;
     }
