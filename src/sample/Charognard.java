a014ce110cfdb82e13ed3e0444cbbb0191a3e410
JoelAubin <joel.mendjemen@ensta-bretagne.org>
2019-12-13 16:32:55 +0100

    Update Charognard.java
    corrections des  commentaires

--- src/sample/Charognard.java
+++ src/sample/Charognard.java
@@ -4,4 +4,14 @@ public class Charognard extends Animal {
     public Charognard(int nbFaim, int nbSoif, int x, int y, int vitm, int perc, int end){
         super(nbFaim, nbSoif, x, y, vitm, perc, end);
     }
+    public int cherchecadavres(ArrayList <Animal> ch ) {
+        /** Fonction à appeler dès que le charognard cherche un cadavre non décomposé à manger:
+         * Il vérifie si la cible le niveau de vie est le minimum possible + le niveau de décomposition minimum
+         * il tient aussi compte du danger rsique d'être lui aussi manger par un canivore
+         * Entrees : liste des animaux
+         * Sorties : True si cible et l'ID de la cible/ False sinon et 0
+         *
+         */
+
+    }
 }
