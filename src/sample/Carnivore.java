c636cd9418708aad3c348a0f6e0ddff2487bb390
Sh4wy <anthonin.martinel@ensta-bretagne.org>
2019-12-13 16:45:27 +0100

    Update_1212_1645
    Update 12/12 16h45 : creation classe poursuivre cible

--- src/sample/Carnivore.java
+++ src/sample/Carnivore.java
@@ -1,8 +1,8 @@
 package sample;
 import java.util.*;
-import java.util.ListIterator;
-import java.util.LinkedList;
-import math.h;
+import java.lang.Math;
+
+
 
 
 /** ATTENTION : valeurs à modifier par leur valeurs numériques :
@@ -17,7 +17,7 @@ public class Carnivore extends Animal {
         super(ValnbFaim, ValnbSoif, x, y);
     }
 
-    public int chercheProie(ArrayList <Animal> a ) {
+    public int chercheProie(ArrayList <Animal> animaux ) {
         /** Fonction a appeler des que le carnivore cherche une prois à manger:
          * Il choisit sa cible en prenant la proie avec le produit endurance*distance le plus faible
          * Entrees : liste des animaux
@@ -28,35 +28,38 @@ public class Carnivore extends Animal {
         /** ID de la cible en cours */
         Herbivore cible;
 
-        /** booleen pour savoir si on trouve une cible */
-        int test_cible = 0;
-
         /** enduranceCible*distance min */
-        int min = 100000;
+        double min = 100000;
         /** valeur produit  enduranceCible*distance A DEFINIR */
-        int valProduit = 0;
+        double valProduit = 0;
 
         /** distance cible-proie */
-        int dist = 0;
+        double dist = 0;
 
         /** l'id de la cible choisie */
-        int ID = 0;
+        char ID = '-1';
 
         // Parcours de la liste des animaux
-        lIterator = listeAnimaux.listIterator();
-        while (lIterator.hasNext()) {
-            cible = lIterator.next();
-            dist = pow(pow(this.abscisse - cible.abscisse, 2) + pow(this.ordonnee - cible.ordonnee, 2), 0.5);
+        for (int counter = 0 ; counter < animaux.size() ; counter++) {
+            cible = animaux.get(counter);
+            dist = Math.pow(Math.pow(this.abscisse - cible.abscisse, 2) + Math.pow(this.ordonnee - cible.ordonnee, 2), 0.5);
             if (dist < this.perception) {
                 valProduit = dist * cible.endurance;
                 if (valProduit < min) {
-                    test_cible = 1;
                     ID = cible.getid();
                     min = valProduit;
                 }
             }
         }
-        return test_cible, ID;
+        return ID;
+    }
+
+    public void poursuivre_cible(Herbivore cible) {
+        /** Cas de l'animal idiot qui court simplement vers la position de la cible */
+        double dist = Math.sqrt(Math.pow(cible.abscisse - this.abscisse), 2)
+        double x = (cible.abscisse - this.abscisse) * this.vitesse / dist;
+        this.deplacer(x,y);
+
     }
 
 
