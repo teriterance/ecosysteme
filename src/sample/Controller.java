a7315e422b6c0a8db257f2b0075391b3a0be3a04
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2020-01-04 11:35:19 +0100

    bonjour controlleur fonctionnnel

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,41 +4,51 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Controller {
-    public static void main(String[] args) {
-        /* étapes de fonctionnment
-        - on creait une liste d'annimal de type Carnivore charognard et herbivore
-        - On les fait évoluer en appelant les differentes fonctions qui sont contenues dans ces derniers
-        - On affiche le resultat
-         */
+    private ArrayList<Carnivore> listeCarnivore;
+    private ArrayList<Herbivore> listeHerbivore;
+    private ArrayList<Charognard> listeCharognard;
+    private ArrayList<Point_eau> listePoint_eau;
 
-        //on génere une liste de carnivore en fonction sur le terain
-        ArrayList<Carnivore> Carnivores = new ArrayList<Carnivore>();
-        Random ran = new Random();
+    public ArrayList<Carnivore> get_list_carnivore(){
+        return (ArrayList<Carnivore>) this.listeCarnivore.clone();
+    }
 
-        //on rempli la liste
-        for( int i =0; i< 1; i++){
-            Carnivores.add(new Carnivore(52,52));
+    public ArrayList<Herbivore> get_list_herbivore(){
+        return (ArrayList<Herbivore>) this.listeHerbivore.clone();
+    }
+
+    public ArrayList<Point_eau> get_list_Point_eau(){
+        return (ArrayList<Point_eau>) this.listePoint_eau.clone();
+    }
+
+    public void gen_ecosyst(int nb_carnivore, int nb_herbivore, int nb_charognard, int nb_eau, int taille_eau_max,int taille_ter){
+        Random rd = new Random();
+        for( int i =0; i< nb_carnivore; i++){
+            this.listeCarnivore.add(new Carnivore(rd.nextInt(taille_ter),rd.nextInt(taille_ter), taille_ter));
         }
-
-        ArrayList<Herbivore> Herbivores = new ArrayList<Herbivore>();
-        for( int i =0; i< 1; i++){
-            Herbivores.add(new Herbivore(54,54));
+        for( int i =0; i< nb_herbivore; i++){
+            this.listeHerbivore.add(new Herbivore(rd.nextInt(taille_ter),rd.nextInt(taille_ter), taille_ter));
         }
+        //a rajouter une initiallisation de charognard
 
-        ArrayList<Point_eau> eaus = new ArrayList<Point_eau>();
-
-        for( int i =0; i< 1; i++){
-            eaus.add(new Point_eau(50,50, 2));
+        for( int i =0; i< nb_eau; i++){
+            this.listePoint_eau.add(new Point_eau(rd.nextInt(taille_ter),rd.nextInt(taille_ter),rd.nextInt(taille_eau_max)));
         }
-        int i = 0;
-        while (i < 5){
-            i++;
-            for (int counter = 0 ; counter < Carnivores.size() ; counter++) {
-                Carnivores.get(counter).vivre(eaus, Herbivores);
-            }
-            for (int counter = 0 ; counter < Herbivores.size() ; counter++) {
-                Herbivores.get(counter).vivre(eaus, Carnivores);
-            }
+    }
+
+    public void reinitialiser(){
+        this.listeCarnivore.clear();
+        this.listeHerbivore.clear();
+        this.listeCharognard.clear();
+        this.listePoint_eau.clear();
+    }
+
+    public void unTour(){
+        for (int counter = 0 ; counter < this.listeCarnivore.size() ; counter++) {
+            this.listeCarnivore.get(counter).vivre(this.listePoint_eau, this.listeHerbivore);
+        }
+        for (int counter = 0 ; counter < this.listeHerbivore.size() ; counter++) {
+            this.listeHerbivore.get(counter).vivre(this.listePoint_eau, this.listeCarnivore);
         }
     }
 }
